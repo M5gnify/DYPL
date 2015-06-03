ad62477e0d26f7e5ff0ca12c255597bee32afe75
Jonathan-92 <jonte790@gmail.com>
2015-06-03 11:08:13 +0200

    added playlists_structure.txt

--- projects/gothonweb/test2.py
+++ projects/gothonweb/test2.py
@@ -8,12 +8,12 @@ username = "masterjonis"
 
 sp = spotipy.Spotify(auth=token.token)
 playlists = sp.user_playlists(username)["items"]
-
-for playlist in playlists:
-    print playlist['name']
-    for trackItem in sp.user_playlist_tracks(username, playlist['id'])['items']:
-        parse(trackItem)
-    print
+parse( playlists)
+# for playlist in playlists:
+#     print playlist['name']
+#     for trackItem in sp.user_playlist_tracks(username, playlist['id'])['items']:
+#         parse(trackItem)
+#     print
     
 # parseTreeer.parse(playlists)
 #sp.user_playlist_remove_all_occurrences_of_tracks(username, "5wEqYYiT6roVtkdF75bMvM", ["2HbVINmehEbPmjlcueXEJJ", "40Gxnw5Vc8hnhGFknXHe3R"])
